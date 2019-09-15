16403ac1069f603527175ecd7c43151ca2d3b8b8
SamJ2014 <37620490+SamJ2018@users.noreply.github.com>
2019-09-15 22:23:09 +0800

    fix

--- src/main/java/com/cys/blog/controller/admin/TypeController.java
+++ src/main/java/com/cys/blog/controller/admin/TypeController.java
@@ -1,5 +1,6 @@
 package com.cys.blog.controller.admin;
 
+import com.cys.blog.pojo.Type;
 import com.cys.blog.service.TypeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
@@ -7,8 +8,13 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.Valid;
 
 /**
  * @author sam
@@ -30,7 +36,24 @@ public class TypeController {
     }
 
     @GetMapping("/types/input")
-    public String input(){
+    public String input(Model model) {
+        model.addAttribute("type", new Type());
         return "admin/types-input";
+    }
+
+    @PostMapping("/types")
+    public String post(@Valid Type type, BindingResult result, RedirectAttributes attributes) {
+        if (result.hasErrors()) {
+            System.out.println(result);
+            return "admin/types-input";
+        }
+        Type t = typeService.saveType(type);
+        if (t == null) {
+            //
+            attributes.addFlashAttribute("message", "操作失败");
+        } else {
+            attributes.addFlashAttribute("message", "操作成功");
+        }
+        return "redirect:/admin/types";
     }
 }
